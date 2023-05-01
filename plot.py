4140b5de4b324e9162b9ce34b0119fab86674897
Minguk Gaang <minguk.gaang@gmail.com>
2023-05-01 16:04:39 +0200

    plot gut

--- plot.py
+++ plot.py
@@ -3,28 +3,43 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-df = pd.read_csv('../assets/data.csv', header=None)
-cols = ['diagnosis'] + [f'feature_{i}' for i in range(1, 31)]
-df = df.drop(df.columns[0], axis=1)
-df.columns = cols
-print(df.head())
+def histogram(df):
+    feature_cols = df.columns[1:]
+    fig, axes = plt.subplots(5, 6, figsize=(15, 9))
 
-feature_cols = df.columns[1:]
+    # Create a histogram of each feature and color-code based on M/B column
+    for i, col in enumerate(feature_cols):
+        ax = axes[i // 6][i % 6]
+        sns.histplot(data=df, x=col, hue=df.columns[0], ax=ax, element="poly", stat="density", common_norm=False)
+        ax.set(xlabel=None, ylabel=None)
+        ax.set_title(f'Feature {i + 1}')
 
-fig, axes = plt.subplots(5, 6, figsize=(15, 9))
+    plt.subplots_adjust(left=0.1, bottom=0.05, right=0.9, top=0.95)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-# Create a histogram of each feature and color-code based on M/B column
-for i, col in enumerate(feature_cols):
-    ax = axes[i // 6][i % 6]
-    sns.histplot(data=df, x=col, hue=df.columns[0], ax=ax, element="poly", stat="density", common_norm=False)
-    ax.set(xlabel=None, ylabel=None)
-    ax.set_title(f'Feature {i + 1}')
+    plt.savefig('../assets/histogram.png')
 
-plt.subplots_adjust(left=0.1, bottom=0.05, right=0.9, top=0.95)
-plt.subplots_adjust(wspace=0.4, hspace=0.4)
+def box_plot(df):
+    feature_cols = df.columns[1:]
+    fig, axes = plt.subplots(5, 6, figsize=(15, 9))
 
-plt.show()
+    # Create a histogram of each feature and color-code based on M/B column
+    for i, col in enumerate(feature_cols):
+        ax = axes[i // 6][i % 6]
+        sns.boxplot(x='diagnosis', y=col, hue='diagnosis',data=df, ax=ax)
+        ax.set(xlabel=None, ylabel=None)
+        ax.set_title(f'Feature {i + 1}')
 
-# X, y = df.iloc[:, 1:].values, df.iloc[:, 0].values
-# print(X)
-# print(y)
+    plt.subplots_adjust(left=0.1, bottom=0.05, right=0.9, top=0.95)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+    plt.savefig('../assets/box_plot.png')
+
+if __name__ == '__main__':
+    df = pd.read_csv('../assets/data.csv', header=None)
+    cols = ['diagnosis'] + [f'feature_{i}' for i in range(1, 31)]
+    df = df.drop(df.columns[0], axis=1)
+    df.columns = cols
+
+    histogram(df)
+    box_plot(df)
